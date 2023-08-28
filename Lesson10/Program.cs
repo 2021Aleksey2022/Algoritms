86c0c3dd57daa3711eccb7ba1ab8cb59bc306214
Алексей <manay.ru@mail.ru>
2023-08-28 10:09:05 +1000

    Тимофей Хирьянов Урок 10

---
+++ Lesson10/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10
+{
+    class Program
+    {
+        static void  Main(string[] args)
+        {
+            Program program = new Program();
+            int n;
+            Console.Write("Enter length to generate  Permutations: ");
+            n = int.Parse(Console.ReadLine());
+            if(n > 20)
+            {
+                Console.WriteLine("Вы ввели слишком большое число");
+            }
+            else
+            {
+                Console.WriteLine("Вы велли ноль или отрицательное число");
+            }
+            int[] buffer = new int[n];
+            bool[] used = new bool[n];
+            program.Permutations(n, 0, buffer, used);
+            Console.ReadLine();
+        }
+        public void Permutations(int number, int current, int[] buffer, bool[] used)
+        {
+            if (current == number)
+            {
+                for(int i = 0; i < number; i++)
+                {
+                    Console.Write($"{buffer[i]}\t");
+                }
+            }
+            else
+            {
+                for(int variant = 0; variant < number; variant++)
+                {
+                    if (!used[variant])
+                    {
+                        buffer[current] = variant;
+                        used[variant] = true;
+                        Permutations(number, current + 1, buffer, used);
+                        used[variant] = false;
+                    }
+                }
+            }
+        }
+        
+    }
+    
+}
---
+++ Lesson10Merge_Sort/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson10Merge_Sort
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            Program program = new Program();
+            program.InputArray(a[n], n);
+            program.MergeSort(a[n], n);
+            program.PrintArray(a[n], n);
+        }
+        public void MergeSort(int array, int arraySize)
+        {
+            //Базовый корпус.Крайне важно внедрить его!!!
+            if (arraySize <= 1) return;
+
+            int middle = arraySize / 2;
+            int[] left = new int[array];
+            int[] right = new int [array + middle];
+
+            //Остерегаться!!!.Используя адресную арифметику : указатель + число дает начало смещенному массиву
+            int leftSize = middle;
+            int rightSize = arraySize - leftSize;
+
+            //Рекурсия идет туда:
+            MergeSort(left[array], leftSize);
+            MergeSort(right[array + middle], rightSize);
+
+            //Объедините эти две уже отсортированные части массива:
+            int[] buffer = new int[arraySize];
+            int left_i = 0;
+            int right_i = 0;
+            int buffer_i = 0;
+            while(left_i < leftSize && right_i < rightSize)
+            {
+                if(left[left_i] <= right[right_i])
+                {
+                    //Берем меньшее значение из левой части массива.
+                    buffer[buffer_i] = left[left_i];
+                    left_i++;
+                    buffer_i++;
+                }
+                else
+                {
+                    //Берем меньшее значение из правой части массива.
+                    buffer[buffer_i] = right[right_i];
+                    right_i++;
+                    buffer_i++;
+                }               
+            }
+            //Копирование левых элементов с левого размера (если что-то осталось).
+            while(left_i < leftSize)
+            {
+                buffer[buffer_i] = left[left_i];
+                left_i++;
+                buffer_i++;
+            }
+            //Копирование правых элементов с правого размера (если что-то осталось).
+            while (right_i < rightSize)
+            {
+                buffer[buffer_i] = right[right_i];
+                right_i++;
+                buffer_i++;
+            }
+            //Копирование из буфера в исходный массив.
+            int[] arrays = new int[array];
+            for (int i = 0; i < arraySize; i++)
+            {               
+                arrays[i] = buffer[i];
+            }
+        }
+        public void InputArray(int a, int n)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                n = int.Parse(Console.ReadLine());
+                
+            }
+        }
+        public void PrintArray(int a, int n)
+        {
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{0}", i);
+            }
+        }
+    }
+}
